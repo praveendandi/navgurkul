1b1b26fd4a85b950ced7bf3b93111b5015324938
navinreddy <navinreddy.a@caratred.com>
2024-05-27 18:01:06 +0530

    first push

---
+++ navgurukul_app/navgurukul/doctype/policies_module/policies_module.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, nithinreddy and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Policies Module", {
+// 	refresh(frm) {
+
+// 	},
+// });
